fa621585c6f7c57c91f0a09f9379452116b7c7c5
Aleksandar Stojanovic <31828736+AleksandarStojanovic91@users.noreply.github.com>
2020-07-25 19:23:00 +0200

    Add files via upload

---
+++ Vezba12/src/Card.java
@@ -0,0 +1,15 @@
+public class Card {
+    public double balance;
+
+    public Card (double balance){
+        this.balance = balance;
+    }
+    public void charge(double charge){
+        if(this.balance>=charge){
+            this.balance -= charge;
+            System.out.println("Uspesno naplaceno: "+charge+" dinara.");
+        } else {
+            System.out.println("Nedovoljno sredstava na racunu, naplata otkazana.");
+        }
+    }
+}
